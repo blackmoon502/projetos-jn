11ca87166f55f0e1c2494b3275b474e378bdc305
blackmoon502 <g21472333@gmail.com>
2025-03-24 07:58:02 -0300

    atualização da cor

---
+++ scripts/operaçoes-basicas.js
@@ -0,0 +1,14 @@
+function operacoesBasicas(){
+    var a = 35
+var b = 66
+
+var soma = a + b
+var subtracao = a - b
+var multiplicacao = a * b
+var divisao = a / b
+
+alert("resultado: "+soma)
+alert("resultado: "+subtracao)
+alert("resultado: "+multiplicacao)
+alert("resultado: "+divisao)
+}
